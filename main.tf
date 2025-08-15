4c80a0e7a2a9f4aa6c7f8c3729a4269d2d74c0c0
GiMerguizo <giovana.pontes5654@hotmail.com>
2025-08-15 18:55:35 -0300

    update Files monitoramento

--- main.tf
+++ main.tf
@@ -34,11 +34,6 @@ resource "docker_container" "postgres" {
     "POSTGRES_DB=${var.db_name}"
   ]
 
-  ports {
-    internal = 5432
-    external = 5432
-  }
-
   networks_advanced {
     name = docker_network.app_network.name
   }
@@ -51,6 +46,13 @@ resource "docker_container" "postgres" {
   volumes {
     host_path      = abspath("${path.module}/sql/script.sql")
     container_path = "/docker-entrypoint-initdb.d/script.sql"
+  }
+
+  healthcheck {
+    test     = ["CMD-SHELL", "pg_isready -U ${var.db_user} -d ${var.db_name}"]
+    interval = "10s"
+    timeout  = "5s"
+    retries  = 5
   }
 }
 
@@ -75,8 +77,8 @@ resource "docker_container" "backend_app" {
     "port=${var.backend_port}",
     "user=${var.db_user}",
     "pass=${var.db_password}",
-    "db_port=${var.db_port}",
-    "host=${var.db_host}",
+    "db_port=5432",
+    "host=${docker_container.postgres.name}",
     "db_name=${var.db_name}"
   ]
 
@@ -106,4 +108,95 @@ resource "docker_container" "frontend" {
   }
 
   depends_on = [docker_container.backend_app]
+}
+
+# Configuração do Monitoramento
+## Configuração do Nginx Exporter
+resource "docker_image" "nginx_exporter_image" {
+  name = "nginx/nginx-prometheus-exporter:latest"
+}
+
+resource "docker_container" "nginx_exporter" {
+  image = docker_image.nginx_exporter_image.image_id
+  name  = "nginx_exporter"
+  restart = "always"
+
+  networks_advanced {
+    name = docker_network.app_network.name
+  }
+
+  env = [
+    "NGINX_SCRAPE_URI=http://frontend:80/stub_status"
+  ]
+
+  depends_on = [docker_container.frontend]
+}
+## Volumes para os dados do Prometheus e Grafana
+resource "docker_volume" "prometheus_data" {
+  name = "prometheus_data"
+}
+
+resource "docker_volume" "grafana_data" {
+  name = "grafana_data"
+}
+
+## Imagem do Prometheus
+resource "docker_image" "prometheus_image" {
+  name = "prom/prometheus:latest"
+}
+
+## Container do Prometheus
+resource "docker_container" "prometheus_app" {
+  image = docker_image.prometheus_image.image_id
+  name  = "prometheus_app"
+  restart = "always"
+
+  ports {
+    internal = 9090
+    external = 9090
+  }
+
+  networks_advanced {
+    name = docker_network.app_network.name
+  }
+
+  volumes {
+    host_path = abspath("${path.module}/prometheus")
+    container_path = "/etc/prometheus"
+  }
+
+  volumes {
+    volume_name = docker_volume.prometheus_data.name
+    container_path = "/prometheus"
+  }
+
+  depends_on = [docker_container.nginx_exporter]
+}
+
+## Imagem do Grafana
+resource "docker_image" "grafana_image" {
+  name = "grafana/grafana:10.1.5"
+}
+
+## Container do Grafana
+resource "docker_container" "grafana_app" {
+  image = docker_image.grafana_image.image_id
+  name  = "grafana_app"
+  restart = "always"
+
+  ports {
+    internal = 3000
+    external = 3001
+  }
+
+  networks_advanced {
+    name = docker_network.app_network.name
+  }
+
+  volumes {
+    volume_name = docker_volume.grafana_data.name
+    container_path = "/var/lib/grafana"
+  }
+
+  depends_on = [docker_container.prometheus_app]
 }
